c8acf138b3a90fffe3d7d20e4ddcd7a25ef033dc
panteng <ptis307@naver.com>
2026-07-31 13:54:41 +0900

    changed routing strategy

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueRouter from 'vue-router/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/skala-vue/',
   plugins: [
+    vueRouter(),
     vue(),
     vueDevTools(),
   ],
